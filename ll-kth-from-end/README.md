40b4376b42aeae2368b3afe3f70b0c04508fba71
tganyan <tganyan@gmail.com>
2018-09-18 15:36:01 -0700

    adding js, tests, readme, asset(s)

---
+++ ll-kth-from-end/README.md
@@ -0,0 +1,11 @@
+# LL Kth from End
+
+Write a function that takes a number value and a linked list as arguments. Using the number argument, count back x amount of times from the end of the linked list and return the value of that node.
+
+## Running the tests
+
+1. Testing to see if K is a number
+2. Testing to check if list is empty
+3. Testing to check if K is bigger than list length
+
+![Whiteboarding image](assets/ll-kth-from-end.jpg)
---
+++ ll-kth-from-end/src/linked-list.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const Node = require('./node');
+
+module.exports = class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertAtHead(value) {
+    const node = new Node(value);
+
+    node.next = this.head;
+    this.head = node;
+    return this;
+  }
+
+  find(value) {
+    if (!this.head) {
+      throw new Error('__ERROR__ The list is empty!');
+    }
+
+    if (this.head.value === value) {
+      return this.head;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode.next) {
+      if (currentNode.next.value === value) {
+        return currentNode.next;
+      }
+
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+
+  remove(value) {
+    if (!this.head) {
+      throw new Error('__ERROR__ The list is empty!');
+    }
+
+    if (this.head.value === value) {
+      this.head = this.head.next;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode.next) {
+      if (currentNode.next.value === value) {
+        currentNode.next = currentNode.next.next;
+        return currentNode.next;
+      }
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
+};
+
