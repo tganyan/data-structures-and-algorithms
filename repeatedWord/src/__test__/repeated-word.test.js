4b405ddd78291ed0145f0846e223a8ccce0926bd
tganyan <tganyan@gmail.com>
2018-10-16 16:52:16 -0700

    adding one more test

--- repeatedWord/src/__test__/repeated-word.test.js
+++ repeatedWord/src/__test__/repeated-word.test.js
@@ -4,6 +4,7 @@ const repeatedWord = require('../repeated-word');
 
 const testString = 'A great day this has turned out to be. I\'m suicidal, me mate tries to kill me, me gun gets nicked and we\'re still in fookin\' Bruges!';
 const emptyString = '';
+const noRepeat = 'Ay ee eye oh you';
 
 describe('#repeated-word.js', () => {
   test('is the correct word returned', () => {
@@ -11,5 +12,8 @@ describe('#repeated-word.js', () => {
   });
   test('is it returning undefined if empty string', () => {
     expect(repeatedWord.repeated(emptyString)).toEqual(undefined);
+  });
+  test('does it return undefined is no repeated word', () => {
+    expect(repeatedWord.repeated(noRepeat)).toEqual(undefined);
   });
 });
