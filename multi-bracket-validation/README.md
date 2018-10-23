5b635025aa9e49c16ecf60a03c48ae552e70510a
tganyan <tganyan@gmail.com>
2018-10-22 17:00:50 -0700

    adding updated js and tests... this is not working and I have no idea why

---
+++ multi-bracket-validation/README.md
@@ -0,0 +1,11 @@
+# Repeated Word
+
+Write a function that takes in a string and returns true if the brackets in the string are correctly formatted.
+
+## Running the tests
+
+1. Testing to ensure truthy return on a properly formatted string
+2. Testing to ensure falsey return on an improperly formatted string
+3. Testing to ensure falsey return if string is empty
+
+![Whiteboarding image](assets/multi-bracket-validation.jpg)
---
+++ multi-bracket-validation/src/multi-bracket-validation.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const multiBrackets = module.exports = {};
+
+class Stack {
+  constructor() {
+    this._container = [];
+  }
+
+  push(value) {
+    this._container.push(value);
+  }
+
+  pop() {
+    return this._container.pop();
+  }
+}
+
+multiBrackets.validator = (string) => {
+  let myStack = new Stack();
+  let returnValue = false;
+  let poppedVal = null;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === '[' || string[i] === '{' || string[i] === '(') {
+      myStack.push(string[i]);
+      poppedVal = myStack.pop();
+      switch (poppedVal) {
+        case '{':
+          if (string[string.length - i] === '}') {
+            returnValue = true;
+          }
+          break;
+        case '(':
+          if (string[string.length - i] === ')') {
+            returnValue = true;
+          }
+          break;
+        case '[':
+          if (string[string.length - i] === ']') {
+            returnValue = true;
+          }
+          break;
+        default:
+          returnValue = false;
+      }
+      console.log(poppedVal + 'pop');
+      console.log(string[(string.length - 1) - i] + 'string');
+    }
+  }
+  return returnValue;
+}
+
+const testOne = multiBrackets.validator('{[(text)]}');
+// const testTwo = multiBrackets.validator('{[(text)]}}}}');
+// const testThree = multiBrackets.validator('{[(text)))]}');
+
+// multiBrackets.validator('{[(text)]}}}}');
+// multiBrackets.validator('{[(text)]}');
+
+console.log(testOne);
+// console.log(testTwo);
+// console.log(testThree);
